9ed8500ed7c85be06d38514d441009e98dead3e8
Himanshu Sharma <himanshusharma96@gmail.com>
2025-05-29 21:16:00 -0500

    Hydra config test works now

--- discord_src/config/app_config.py
+++ discord_src/config/app_config.py
@@ -2,20 +2,17 @@ import dataclasses
 from typing import Optional
 
 # Pydantic has more validation features, plus it gives us static and runtime type safety.
-from pydantic import Field
+from pydantic import Field, field_validator
 from pydantic.dataclasses import dataclass
 
-from omegaconf import MISSING
+from omegaconf import MISSING, DictConfig, OmegaConf
+from typing import Any, Dict, List, Type
 
 import hydra  # other option was to use the bison package
 from hydra.core.config_store import ConfigStore
 
 @dataclass
 class ModelServerParams:
-  # sd_server: str = MISSING
-  # sd_port: int = MISSING
-  # sd_uri: str = MISSING
-
   stable_diffusion_server: str = MISSING
   stable_diffusion_port: int = MISSING
   stable_diffusion_uri: str = MISSING
@@ -35,25 +32,31 @@ class AppConfig:
   model_server: ModelServerParams = dataclasses.field(default_factory = ModelServerParams)  # model params
   paths: ProjPaths = dataclasses.field(default_factory = ProjPaths)
 
+  @field_validator("LOG_LEVEL")
+  @classmethod
+  def validate_log_level(cls, v: str) -> str:
+    log_options = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+    if v not in log_options:
+      raise ValueError(f"'LOG_LEVEL' must be from this list: {log_options} but got '{v}'")
+    return v
+
+  
+  @classmethod
+  def from_dict(cls, cfg: Dict[str, Any])):
+      return cls(**OmegaConf.to_container(cfg, resolve=True))
 
 cs = ConfigStore.instance()
 cs.store(name = 'app_config', node = AppConfig)
-# cs.store(name="config2", node=MySQLConfig(host="test.db", port=3307))
-
 
 # Testing code here
-# @hydra.main(version_base = None, config_path='.', config_name="config")
-# def do_this(cfg: AppConfig):
-#     # print(f'cfg: {cfg}')
-#     # return
-#     # this line actually runs the checks of pydantic
-#     from omegaconf import OmegaConf
-#     OmegaConf.to_object(cfg)
-#     # log to console and into the `outputs` folder per default
-#     print(f"\n{OmegaConf.to_yaml(cfg)}")
+@hydra.main(version_base = None, config_path='.', config_name="config")
+def get_config(cfg: DictConfig):
+    # print(f'Loaded config: {OmegaConf.to_yaml(cfg)}')
+    # return OmegaConf.to_object(cfg)
+    validated_config = AppConfig(**OmegaConf.to_container(cfg, resolve=True))
+    print(type(validated_config))
+    # return validated_config
 
-#     print(f'habiibi: {cfg.paths.data_path}')
-
-# if __name__ == '__main__':
-#   print(f'hi. erer')
-#   do_this()
+if __name__ == '__main__':
+  print(f'hi. erer')
+  get_config()
